f8598133af96e0b6a1c515498e54dd08d6ff3726
abircse45 <abiruodacse@gmail.com>
2023-03-28 09:07:04 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -71,7 +71,7 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       title: 'Bye Bye Cry',
       theme: ThemeData(fontFamily: 'Neue Einstellung'),
-      home: StartPage(),
+      home: InitialHomePage(),
     );
   }
 }
